f637af82b0504eb24da9f6707bac1b5768a0dfd5
Feng Zhang <85175269+zhangfeng-2021@users.noreply.github.com>
2022-07-25 09:49:40 +0800

    Add files via upload

--- active predicted based on different thresholds.py
+++ active predicted based on different thresholds.py
@@ -6,21 +6,55 @@ Created on Tue Jul 13 21:50:53 2021
 """
 import pandas as pd
 
-with open(r"Toxi_infor_sum.csv", encoding = None, errors="ignore") as fin:
+with open(r"in-silico toxicity and drug interaction prediction\integration\Toxi_infor_sum.csv", encoding = None, errors="ignore") as fin:
     df1 = pd.read_csv(fin)
-
-similarity = 0.7 
-RepreComList1= list(set(df1["Active compound name"]))[0:]
-mask1 = (df1["Similarity_score"] > similarity) & (df1["Active compound name"].isin(RepreComList1))
+    
+similarity = 1
+RepreComList1= list(set(df1["Active_compound_name"]))[0:]
+mask1 = (df1["Similarity_score"] > similarity) & (df1["Active_compound_name"].isin(RepreComList1))
 df2 = df1[mask1]
-activenumber1 = len(set(df2["Active compound name"]))
-print(activenumber1)
+activenumber1 = len(set(df2["Active_compound_name"]))
+print("Number of active compounds with toxicity retrieved: ", str(activenumber1), " with the similarity threshold of "+ str(similarity))
+    
 
-similarity = 0.561 
-RepreComList1= list(set(df1["Active compound name"]))[0:]
-mask1 = (df1["Similarity_score"] > similarity) & (df1["Active compound name"].isin(RepreComList1))
+similarity = 0.759 
+RepreComList1= list(set(df1["Active_compound_name"]))[0:]
+mask1 = (df1["Similarity_score"] > similarity) & (df1["Active_compound_name"].isin(RepreComList1))
 df2 = df1[mask1]
-activenumber1 = len(set(df2["Active compound name"]))
-df3 = df2.drop_duplicates(subset=["Active compound name", "PubChem CID of similar compounds"], keep='first', inplace=False)
-df3.to_csv(r"prediction_0.561.csv")
-print(activenumber1)
+activenumber1 = len(set(df2["Active_compound_name"]))
+print("Number of active compounds with toxicity retrieved: ", str(activenumber1), " with the similarity threshold of "+ str(similarity))
+
+similarity = 0.6171 
+RepreComList1= list(set(df1["Active_compound_name"]))[0:]
+mask1 = (df1["Similarity_score"] > similarity) & (df1["Active_compound_name"].isin(RepreComList1))
+df2 = df1[mask1]
+activenumber1 = len(set(df2["Active_compound_name"]))
+df3 = df2.drop_duplicates(subset=["Active_compound_name", "PubChem_CID"], keep='first', inplace=False)
+df3.to_csv(r"prediction_"+str(similarity)+".csv")
+print("Number of active compounds with toxicity retrieved: ", str(activenumber1), " with the similarity threshold of "+ str(similarity))
+
+
+with open(r"in-silico toxicity and drug interaction prediction\integration\similar_comp_properties_sum.csv", encoding = None, errors="ignore") as fin:
+    df1 = pd.read_csv(fin)
+    
+similarity = 1 
+RepreComList1= list(set(df1["Active_compound_name"]))[0:]
+mask1 = (df1["Similarity_score"] > similarity) & (df1["Active_compound_name"].isin(RepreComList1))
+df2 = df1[mask1]
+activenumber1 = len(set(df2["Active_compound_name"]))
+print("Number of active compounds with similar compounds retrieved: ", str(activenumber1), " with the similarity threshold of "+ str(similarity))
+
+similarity = 0.759 
+RepreComList1= list(set(df1["Active_compound_name"]))[0:]
+mask1 = (df1["Similarity_score"] > similarity) & (df1["Active_compound_name"].isin(RepreComList1))
+df2 = df1[mask1]
+activenumber1 = len(set(df2["Active_compound_name"]))
+print("Number of active compounds with similar compounds retrieved: ", str(activenumber1), " with the similarity threshold of "+ str(similarity))
+
+similarity = 0.6171 
+RepreComList1= list(set(df1["Active_compound_name"]))[0:]
+mask1 = (df1["Similarity_score"] > similarity) & (df1["Active_compound_name"].isin(RepreComList1))
+df2 = df1[mask1]
+activenumber1 = len(set(df2["Active_compound_name"]))
+print("Number of active compounds with similar compounds retrieved: ", str(activenumber1), " with the similarity threshold of "+ str(similarity))
+
