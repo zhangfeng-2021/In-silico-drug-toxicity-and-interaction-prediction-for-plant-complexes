a0dbaa3f14733aed2d4d9b32ee2a6a23898a3644
Feng Zhang <85175269+zhangfeng-2021@users.noreply.github.com>
2022-07-25 09:49:23 +0800

    Add files via upload

--- drug_interactions_split.py
+++ drug_interactions_split.py
@@ -8,26 +8,31 @@ import pandas as pd
 import ast
 import os
 
-df2 = pd.DataFrame(columns = ["Active_compound_name","PubChem CID of similar compounds","Similarity score",
+threshold = 0.561
+dfintera = pd.DataFrame(columns = ["Active_compound_name","PubChem CID of similar compounds","Similarity_score",
                               "Interactions","Drug_interacted", "Activities","Roles"])
-df3 = pd.DataFrame(columns = ["Active_compound_name","PubChem CID of similar compounds","Similarity score",
+dfins = pd.DataFrame(columns = ["Active_compound_name","PubChem CID of similar compounds","Similarity_score",
                               "Interactions","Drug_interacted", "Activities","Roles"])
 
-with open(r"distinct drug_interaction_0.561.csv", encoding = None, errors="ignore") as fin:
+with open(r"in-silico toxicity and drug interaction prediction\integration\Toxi_infor_sum.csv", encoding = None, errors="ignore") as fin:
     df1 = pd.read_csv(fin)
-    container = [] 
-for inter, cid, name, score in zip(df1["Interactions"], df1["PubChem CID of similar compounds"], 
-                                   df1["Active_compound_name"], df1["Similarity score"]):
+container = []
+df1 = df1[df1["Interactions"].notna()]
+mask = df1["Similarity_score"] > threshold
+df1 = df1[mask] 
+for inter, cid, name, score in zip(df1["Interactions"], df1["PubChem_CID"], 
+                                   df1["Active_compound_name"], df1["Similarity_score"]):
     inter = ast.literal_eval(inter)                                                            
     for j in inter:
         print(j)
         container.append(name)
         container.append(cid)
-        container.append(name)        
         container.append(score)
         container.append(j)
-        df3 = pd.DataFrame([container], columns = ["Active_compound_name","PubChem CID of similar compounds",
-                                                   "Similarity score","Interactions"])
-        df2 = df2.append(df3, ignore_index= True)
-        container = [] 
-df2.to_csv(r"drug_interaction_network_0.561.csv")
+        dfins = pd.DataFrame([container], columns = ["Active_compound_name","PubChem CID of similar compounds",
+                                                   "Similarity_score","Interactions"])
+        dfintera = dfintera.append(dfins, ignore_index= True)
+        container = []
+dfintera.drop_duplicates(subset=(["Active_compound_name", "PubChem CID of similar compounds","Interactions"]), keep= 'first')
+dfintera.to_csv(r"in-silico toxicity and drug interaction prediction\Toxicity and interaction mining\Drug_interaction\drug_interaction_"+str(threshold)+".csv")
+print("Interaction data splitted")
